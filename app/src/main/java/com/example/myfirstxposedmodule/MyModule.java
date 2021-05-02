265ea0ae36667ad79e8f8000061606965afa9919
aeifkz <aeifkz@gmail.com>
2021-05-02 16:35:09 +0800

    初始化 Xposed 專案

---
+++ app/src/main/java/com/example/myfirstxposedmodule/MyModule.java
@@ -0,0 +1,15 @@
+package com.example.myfirstxposedmodule;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MyModule implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        XposedBridge.log("Loaded app: " + loadPackageParam.packageName);
+        XposedBridge.log("I am author aeifkz");
+    }
+
+}
